064849d8cad9c4863057a7cc4166cd92718bcc8c
Adam Badke <mr_spikey@hotmail.com>
2023-04-09 10:26:40 -0700

    Cleaned up the hashed data object implementation

--- Source/HashedDataObject.h
+++ Source/HashedDataObject.h
@@ -15,28 +15,41 @@ namespace en
 		HashedDataObject() : m_dataHash(0) {}
 
 	public:
-		inline uint64_t GetDataHash() const { return m_dataHash; }
+		inline uint64_t GetDataHash() const;
 
-		void AddDataBytesToHash(void const* const data, size_t numBytes)
-		{
-			SEAssert("Invalid data for hash", data != nullptr && numBytes > 0);
-
-			std::hash<uint8_t> hasher;
-			uint8_t const* dataPtr = (uint8_t const*)data;
-			for (size_t curByte = 0; curByte < numBytes; curByte++) // Lifted from Boost hash_combine:
-			{
-				m_dataHash ^= hasher(*dataPtr) + 0x9e3779b9 + (m_dataHash << 6) + (m_dataHash >> 2); 
-				dataPtr++;
-			}
-		}
+		inline void AddDataBytesToHash(void const* const data, size_t numBytes);
 
 		template<typename T>
-		inline void AddDataBytesToHash(T const& data)
-		{
-			AddDataBytesToHash(&data, sizeof(T));
-		}
+		inline void AddDataBytesToHash(T const& data);
 
 	private:
 		uint64_t m_dataHash;
 	};
+
+
+	uint64_t HashedDataObject::GetDataHash() const
+	{
+		return m_dataHash;
+	}
+
+
+	void HashedDataObject::AddDataBytesToHash(void const* const data, size_t numBytes)
+	{
+		SEAssert("Invalid data for hash", data != nullptr && numBytes > 0);
+
+		std::hash<uint8_t> hasher;
+		uint8_t const* dataPtr = (uint8_t const*)data;
+		for (size_t curByte = 0; curByte < numBytes; curByte++) // Lifted from Boost hash_combine:
+		{
+			m_dataHash ^= hasher(*dataPtr) + 0x9e3779b9 + (m_dataHash << 6) + (m_dataHash >> 2);
+			dataPtr++;
+		}
+	}
+
+
+	template<typename T>
+	void HashedDataObject::AddDataBytesToHash(T const& data)
+	{
+		AddDataBytesToHash(&data, sizeof(T));
+	}
 }
