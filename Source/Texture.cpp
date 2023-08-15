d618de5142b40d13dd1262ead9a588711d1f60ed
Adam B <b1skit@users.noreply.github.com>
2023-08-15 09:32:46 -0700

    Removed a deprecated assert

--- Source/Texture.cpp
+++ Source/Texture.cpp
@@ -52,8 +52,7 @@ namespace re
 		SEAssert("Invalid dimension", m_texParams.m_dimension != Texture::Dimension::Invalid);
 		SEAssert("Invalid format", m_texParams.m_format != Texture::Format::Invalid);
 		SEAssert("Invalid color space", m_texParams.m_colorSpace != Texture::ColorSpace::Invalid);
-		SEAssert("Invalid dimensions: Textures must be a minimum of 4x4 to support block compression", 
-			m_texParams.m_width >= 4 && m_texParams.m_height >= 4); 
+		SEAssert("Invalid dimensions", m_texParams.m_width >= 0 && m_texParams.m_height >= 0); 
 
 		platform::Texture::CreatePlatformParams(*this);
 
