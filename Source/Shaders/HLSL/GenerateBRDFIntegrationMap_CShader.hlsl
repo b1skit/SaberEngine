42a120359df2f1175badd7c1f7aefa29b910a8fb
Adam B <b1skit@users.noreply.github.com>
2023-09-18 17:43:09 -0700

    Fixed a bug resulting in NaNs in the DX12 BRDF LUT integration shader

--- Source/Shaders/HLSL/GenerateBRDFIntegrationMap_CShader.hlsl
+++ Source/Shaders/HLSL/GenerateBRDFIntegrationMap_CShader.hlsl
@@ -2,13 +2,17 @@
 #include "SaberComputeCommon.hlsli"
 #include "SaberGlobals.hlsli"
 
-#define NUM_SAMPLES 1024
 
 RWTexture2D<float2> output0 : register(u0);
 
+struct BRDFIntegrationParamsCB
+{
+	uint4 g_integrationTargetResolution;
+};
+ConstantBuffer<BRDFIntegrationParamsCB> BRDFIntegrationParams;
 
+#define NUM_SAMPLES 1024
 
-// TODO: Move these helper functions somewhere appropriate!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 // Helper function: Compute the Van der Corput sequence via radical inverse
 // Based on:  http://holger.dammertz.org/stuff/notes_HammersleyOnHemisphere.html (As per Hacker's Delight)
@@ -32,19 +36,21 @@ float2 Hammersley2D(uint i, uint N)
 }
 
 
-//  Get a sample vector near a microsurface's halfway vector, from input roughness and a the low-discrepancy sequence value
+//  Get a sample vector near a microsurface's halfway vector, from input roughness and a low-discrepancy sequence value
 float3 ImportanceSampleGGX(float2 Xi, float3 N, float roughness)
 {
 	const float a = roughness * roughness;
 
 	const float phi = M_2PI * Xi.x;
-	const float cosTheta = sqrt((1.f - Xi.y) / (1.f + (a * a - 1.f) * Xi.y));
-	const float sinTheta = sqrt(1.f - cosTheta * cosTheta);
 
+	// Need to guard against divide-by-0 or else we get NaNs
+	const float cosTheta = sqrt((1.f - Xi.y) / max(1.f + (a * a - 1.f) * Xi.y, FLT_MIN) );
+	const float sinTheta = sqrt(max(1.f - cosTheta * cosTheta, FLT_MIN));
+	
 	// Convert spherical -> cartesian coordinates
 	const float3 H = float3(
-		cos(phi) * sinTheta,
-		sin(phi) * sinTheta,
+		sinTheta * cos(phi),
+		sinTheta * sin(phi),
 		cosTheta);
 
 	// from tangent-space vector to world-space sample vector
@@ -54,7 +60,7 @@ float3 ImportanceSampleGGX(float2 Xi, float3 N, float roughness)
 
 	const float3 sampleVec = tangent * H.x + bitangent * H.y + N * H.z;
 
-	return normalize(sampleVec);
+	return sampleVec;
 }
 
 
@@ -78,43 +84,46 @@ float GeometrySmith(float NoV, float NoL, float remappedRoughness)
 }
 
 
-
 [numthreads(1, 1, 1)]
 void main(ComputeIn In)
 {
-	const uint2 targetResolution = uint2(1024, 1024); // TODO: Pass this in via a parameter block
+	const uint2 targetResolution = BRDFIntegrationParams.g_integrationTargetResolution.xy;
 	
 	const uint2 texelCoord = In.DTId.xy;
 	
 	float2 screenUV = PixelCoordsToUV(texelCoord, targetResolution.xy);
 	screenUV.y = 1.f - screenUV.y; // Need to flip Y
-
+	
 	const float NoV = screenUV.x;
+	
 	const float NoV2 = NoV * NoV;
 	const float roughness = screenUV.y;
-
-	const float3 V = float3(sqrt(1.f - NoV2), 0.f, NoV);
+	
+	const float3 V = float3(
+		sqrt(1.f - NoV2), // sin
+		0.f,
+		NoV); // cos
 
 	const float3 N = float3(0.f, 0.f, 1.f);
 
-	float fresnelScale = 0.f;
-	float fresnelBias = 0.f;
+	float fresnelScale = 0.f; // A
+	float fresnelBias = 0.f; // B
 
 	for (uint i = 0; i < NUM_SAMPLES; i++)
 	{
 		const float2 Xi = Hammersley2D(i, NUM_SAMPLES);
-		const float3 H = ImportanceSampleGGX(Xi, N, roughness);
-		const float3 L = normalize(2.f * dot(V, H) * H - V);
+		const float3 H = ImportanceSampleGGX(Xi, N, roughness);	
+		const float3 L = 2.f * dot(V, H) * H - V;
 
-		const float NoL = max(L.z, 0.f);
-		const float NoH = max(H.z, 0.f);
-		const float VoH = max(dot(V, H), 0.f);
+		const float NoL = saturate(L.z);
+		const float NoH = saturate(H.z);
+		const float VoH = saturate(dot(V, H));
 
 		if (NoL > 0.f)
 		{
 			const float G = GeometrySmith(NoV, NoL, roughness);
 			const float GVis = (G * VoH) / max((NoH * NoV), FLT_MIN); // Shouldn't be zero, but just in case
-			const float Fc = pow(1.f - VoH, 5.0f);
+			const float Fc = pow(1.f - VoH, 5.f);
 
 			fresnelScale += (1.f - Fc) * GVis;
 			fresnelBias += Fc * GVis;
@@ -122,10 +131,5 @@ void main(ComputeIn In)
 	}
 
 	// Average the results:
-	fresnelScale /= NUM_SAMPLES;
-	fresnelBias /= NUM_SAMPLES;
-	
-	output0[texelCoord] = float2(fresnelScale, fresnelBias);
-	
-	// TODO: Bug here. We're getting some weird artifacts in the final texture
+	output0[texelCoord] = float2(fresnelScale, fresnelBias) / NUM_SAMPLES;
 }
