3770d2b49bcd8b3b0c091dec660e02ff86f74f98
Adam B <b1skit@users.noreply.github.com>
2023-12-30 10:13:56 -0800

    Cleaned up the EngineComponent interface

--- Source/EngineComponent.h
+++ Source/EngineComponent.h
@@ -7,22 +7,19 @@ namespace en
 	// EngineComponent Interface: Functionality common to components in main game loop
 	class EngineComponent
 	{
-	public:
-		EngineComponent() = default;
-		EngineComponent(EngineComponent const&) = default;
-		EngineComponent(EngineComponent&&) = default;
-		EngineComponent& operator=(EngineComponent const&) = default;
-		
-		virtual ~EngineComponent() = 0;
-
-		// EngineComponent interface:
+	public: // EngineComponent interface:
 		virtual void Update(uint64_t frameNum, double stepTimeMs) = 0;
 
 		virtual void Startup() = 0; // We can't control construction order, so this is called to start the object
 		virtual void Shutdown() = 0;
+
+	public:
+		EngineComponent() = default;
+		EngineComponent& operator=(EngineComponent const&) = default;
+		EngineComponent& operator=(EngineComponent&&) = default;
+
+	private: // No copying allowed
+		EngineComponent(EngineComponent const&) = delete;
+		EngineComponent(EngineComponent&&) = delete;
 	};
-
-
-	// We need to provide a destructor implementation since it's pure virtual
-	inline EngineComponent::~EngineComponent() {};
 }
