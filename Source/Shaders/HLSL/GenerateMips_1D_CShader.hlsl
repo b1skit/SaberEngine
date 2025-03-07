8cebae4f3067ab91ca8266e8b5932fe9cfd68097
Adam B <b1skit@users.noreply.github.com>
2025-03-06 16:52:28 -0800

    Removed some deprecated HLSL #define hackery in place of Technique defines

--- Source/Shaders/HLSL/GenerateMips_1D_CShader.hlsl
+++
@@ -1,3 +0,0 @@
-// © 2024 Adam Badke. All rights reserved.
-#define GENERATE_1D_MIPS
-#include "GenerateMips_CShader.hlsl"
--- Source/Shaders/HLSL/GenerateMips_3D_CShader.hlsl
+++
@@ -1,3 +0,0 @@
-// © 2024 Adam Badke. All rights reserved.
-#define GENERATE_3D_MIPS
-#include "GenerateMips_CShader.hlsl"
