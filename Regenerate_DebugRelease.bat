af73e05c221f1b665400636f7e4cf56da532a3ec
b1skit <mr_spikey@hotmail.com>
2025-08-03 14:25:47 -0700

    Created .bat helpers to regenerate C++ and shader code via Droid

---
+++ Regenerate_DebugRelease.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Change working directory to \SaberEngine\SaberEngine\DroidShaderBurner\
+cd /d "%~dp0SaberEngine\DroidShaderBurner"
+
+REM Execute:
+@echo on
+..\..\Build\x64\Release\DroidShaderBurner\DroidShaderBurner.exe -buildconfig DebugRelease -projectRoot D:\Projects\Development\SaberEngine\ -cleanandrebuild
+
+@echo off
+REM Move back to the script directory
+cd ..\..\
+
+REM Pause to see the output
+timeout /t 7
---
+++ Regenerate_Profile.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Change working directory to \SaberEngine\SaberEngine\DroidShaderBurner\
+cd /d "%~dp0SaberEngine\DroidShaderBurner"
+
+REM Execute:
+@echo on
+..\..\Build\x64\Profile\DroidShaderBurner\DroidShaderBurner.exe -buildconfig Profile -projectRoot D:\Projects\Development\SaberEngine\ -cleanandrebuild
+
+@echo off
+REM Move back to the script directory
+cd ..\..\
+
+REM Pause to see the output
+timeout /t 7
